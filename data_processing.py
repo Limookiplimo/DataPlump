75717fcd8bcfb1ee9fd5d9ae92c49d2d16030a3c
Limoo <limoodrice@gmail.com>
2023-03-01 22:21:50 +0300

    Added kafka functionality

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import pyodbc
-import pandas as pd
+from kafka import KafkaProducer, KafkaConsumer
+
 
 # Connection parameters
 server = 'tcp:localhost,1433'
@@ -15,16 +16,35 @@ def ext_src_tbls():
                         SERVER={server};
                         ENCRYPT={Encrypt};
                         DATABASE={database};
-                        UID={username};
+                        UID={username};kafka_bootstrap_servers
                         PWD={password};
     """
     # Establish connection
     conn= pyodbc.connect(conn_string)
-
+    cursor = conn.cursor()
+    #Execute sql query
     sql_query = f"SELECT * FROM Sales"
-    raw_data = pd.read_sql(sql_query, conn)
-    conn.close()
+    cursor.execute(sql_query)
+
+    # Set up Kafka producer
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
     
-    return raw_data
+
+    # Send each row to Kafka topic | Convert it to string
+    for row in cursor.fetchall():
+        message = str(row).encode('utf-8')
+        producer.send('active_data', value = message)
+    
+    producer.flush()
+
+    # Instantiate Kafka consumer to print one message from the topic
+    consumer = KafkaConsumer('active_data', bootstrap_servers='localhost:9092', auto_offset_reset='earliest', enable_auto_commit=True)
+    message = next(consumer)
+    print(message.value.decode())
 
 
+
+    # Close connections
+    producer.close()
+    cursor.close()
+    conn.close()
