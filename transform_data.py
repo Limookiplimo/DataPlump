a54ce9fb442c145287b38d3707494089f8d3dd59
Limoo <limoodrice@gmail.com>
2023-03-07 22:17:31 +0300

    spark mods

--- transform_data.py
+++ transform_data.py
@@ -1,42 +1,66 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, LongType
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import from_json, col, struct
+from pyspark.sql.types import StructType, StructField, LongType, IntegerType, TimestampType
 
-from kafka import KafkaConsumer, KafkaProducer
-import json
+def read_kafka():
+    # create SparkSession
+    spark = (SparkSession
+            .builder
+            .master('local')
+            .appName('wiki-changes-event-consumer')
+            # Add kafka package
+            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2")
+            .getOrCreate())
+    sc = spark.sparkContext
 
-#Kafka topic
-ACTIVE_TOPIC = 'data_extracts'
+    df = (spark
+        .readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092") # kafka server
+        .option("subscribe", "data_extracts") # topic
+        .option("startingOffsets", "earliest") # start from beginning 
+        .load())
+    
+    schema = StructType([StructField("key",StringType(),True),
+                StructField("value",StringType(),True),
+                StructField("topic",StringType(),True),
+                StructField("partition",IntegerType(),True),
+                StructField("offset",LongType(),True),
+                StructField("timestamp",TimestampType(),True),
+                StructField("timestampType",IntegerType(),True)])
+    
+    # Convert binary to string key and value, create schema & DF
+    df1 = (df
+        .withColumn("key", df["key"].cast(StringType()))\
+        .withColumn("value", df["value"].cast(StringType()))
+        .withColumn("value", from_json("value", schema)) #schema
+        .select('*', struct(col("value")).alias("data"))) #dataframe
 
-def transform_data():
-    consumer = KafkaConsumer('data_extracts',
-                    bootstrap_servers='localhost:9092',
-                    auto_offset_reset='earliest',
-                    enable_auto_commit=True)
+    raw_path ="/home/limoo/Documents/projects/DataPlump/data-extracts"
+    checkpoint_path = "/home/limoo/Documents/projects/DataPlump/data-extracts-checkpoint"
 
-    spark = SparkSession.builder.appName("DataPlump").getOrCreate()
-    schema = StructType([
-        StructField("Retailer", StringType(), True),
-        StructField("Retailer ID", IntegerType(), True),
-        StructField("Invoice Date", StringType(), True),
-        StructField("Region", StringType(), True),
-        StructField("State", StringType(), True),
-        StructField("City", StringType(), True),
-        StructField("Price per Unit", StringType(), True),
-        StructField("Units Sold", StringType(), True),
-        StructField("Total Sales", StringType(), True),
-        StructField("Operating Profit", StringType(), True),
-        StructField("Operating Margin", StringType(), True),
-        StructField("Sales Method", StringType(), True)
-])
-    df = spark\
-    .readStream\
-    .format("kafka")\
-    .option("kafka.bootstrap.servers", "localhost:9092")\
-    .option("subscribe", "data_extracts")\
-    .option("startingOffset", "earliest")\
-    .load()\
-    .select(from_json(col("value").cast("string"), schema).alias("data"))\
-    .select("data.*")
+    queryStream = (
+                    df1\
+                    .writeStream\
+                    .format("kafka")\
+                    .queryName("data_transformation")\
+                    .option("checkpointLocation", checkpoint_path)\
+                    .option("path", raw_path)\
+                    .outputMode("append")\
+                    .partitionBy("change_timestamp_date", "server_name")\
+                    .trigger(once=True)\
+                    .start())
+    clean_df = (
+                spark\
+                .readStream\
+                .format("kafka")\
+                .option("kafka.bootstrap.servers", "localhost:9092")\
+                .option("subscribe", "data_extracts")\
+                .schema(df1.schema)\
+                .load())
+    queryStream()
+    clean_df.printSchema()
+    queryStream.stop()
 
-transform_data()
+read_kafka()
