26cba83acb9456c00c347a6078a817edde904512
Limoo <limoodrice@gmail.com>
2023-03-20 22:00:45 +0300

    added loading func

--- data_loading.py
+++ data_loading.py
@@ -1,68 +1,49 @@
-from pyspark.sql import SparkSession
 import psycopg2
+from kafka import KafkaConsumer
 
-def load_data():
-    try:
-        spark = SparkSession.builder.appName("loading").config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2").getOrCreate()
-        df = spark.read.format("kafka") \
-            .option("kafka.bootstrap.servers", "localhost:9092") \
-            .option("subscribe", "clean-data") \
-            .load()
+# set up PostgreSQL connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydb",
+    user="user",
+    password="Mypassword"
+)
 
-        # Convert data to DataFrame and split columns
-        data = df.selectExpr("CAST(value AS STRING)").rdd \
-            .map(lambda x: x[0].split(", ")) \
-            .toDF(["Retailer", "RetailerID", "InvoiceDate", "Region", "State", "City", "Product", "UnitPrice", "Quantity", "Sales", "Profit", "Margin", "Channel"])
-        data.createOrReplaceTempView("clean_data")
-        conn = psycopg2.connect(
-            host="localhost",
-            database="mydb",
-            user="user",
-            password="Mypassword"
-        )
-        cur = conn.cursor()
+# configure Kafka consumer
+consumer = KafkaConsumer(
+    'clean-data', # topic name
+    bootstrap_servers=["localhost:9092"],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+)
 
-        # Load data into PostgreSQL table
-        cur.execute("""
+# insert data into PostgreSQL
+for message in consumer:
+    data = message.value.decode('utf-8')
+    # assuming the data is in a CSV format
+    fields = data.split(',')
+    cur = conn.cursor()
+    cur.execute("""
             CREATE TABLE IF NOT EXISTS Sales (
-                retailer VARCHAR(255),
-                retailer_id VARCHAR(255),
-                invoice_date DATE,
-                region VARCHAR(255),
-                state VARCHAR(255),
-                city VARCHAR(255),
-                product VARCHAR(255),
-                unit_price FLOAT,
-                quantity INTEGER,
-                sales FLOAT,
-                profit FLOAT,
-                margin FLOAT,
-                channel VARCHAR(255)
+                Retailer VARCHAR(255),
+                RetailerID VARCHAR(255),
+                InvoiceDate DATE,
+                Region VARCHAR(255),
+                State VARCHAR(255),
+                City VARCHAR(255),
+                Product VARCHAR(255),
+                UnitPrice VARCHAR(255),
+                Quantity VARCHAR(255),
+                Sales VARCHAR(255),
+                Profit VARCHAR(255),
+                PercentMargin VARCHAR(255),
+                Channel VARCHAR(255)
             );
         """)
-        cur.execute("""
-            DELETE FROM Sales;
-        """)
-        cur.execute("""
-            INSERT INTO Sales
-            SELECT
-                Retailer,
-                RetailerID,
-                TO_DATE(InvoiceDate, 'yyyy-MM-dd'),
-                Region,
-                State,
-                City,
-                Product,
-                CAST(UnitPrice AS FLOAT),
-                CAST(Quantity AS INTEGER),
-                CAST(Sales AS FLOAT),
-                CAST(Profit AS FLOAT),
-                CAST(Margin AS FLOAT),
-                Channel
-            FROM clean_data
-        """)
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print("\nError loading DB: ", e)
-load_data()
+    cur.execute("INSERT INTO Sales( Retailer,RetailerID,InvoiceDate,Region,State,City,Product,UnitPrice,Quantity,Sales,Profit,PercentMargin,Channel) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (fields[0],fields[1],fields[2],fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],fields[9],fields[10],fields[11],fields[12])
+    )
+    conn.commit()
+    cur.close()
+
+conn.close()
